003242dc46918ea498e8540aeab8fdcaba0674a0
AKGHighlander <46221710+AKGHighlander@users.noreply.github.com>
2019-01-04 03:32:26 -0900

    Update Animations.h

--- Animations.h
+++ Animations.h
@@ -51,12 +51,12 @@ extern Kaimana kaimana;
 #define FIREBALL_OFFSET_1    96    // used to create animation -- see code
 #define FIREBALL_DELAY      350    // value in microseconds
 
-void animation_combo_1(void);    // Hadouken (Fireball)
-void animation_combo_2(void);    // Shoryuken (Dragon Punch)
-void animation_combo_3(void);    // Tatsumaki Senpukyaku (Hurricane Kick)
-void animation_combo_4(void);    // Super — Shinkuu Hadouken
-void animation_combo_5(void);    // Ultra 1 — Metsu Hadouken
-void animation_combo_6(void);    // Ultra 2 — Metsu Hadouken
+void animation_combo_1(void);    // PEWGF
+void animation_combo_2(void);    // BDC
+void animation_combo_3(void);    // JFSR
+void animation_combo_4(void);    // f,f+4
+void animation_combo_5(void);    // EWGF
+void animation_combo_6(void);    // OTGF
 
 // other function prototypes
 
